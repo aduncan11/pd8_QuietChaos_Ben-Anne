952aa82c1e9be0b3aa1f07f659a90e300c71e7be
Anne Duncan <anneduncan11@gmail.com>
2014-05-30 14:00:32 -0400

    social things added

--- SocialStory.java
+++ SocialStory.java
@@ -1,7 +1,18 @@
 public class SocialStory extends StoryBit{
 
     public void run(Character c){
+	int social=0;
 	System.out.println("Your friend Veronica needs help with a problem...");
+	System.out.println("Hey, " +c.getName()+" Can you help me out  with something?");
+	System.out.println("yes or no");
+	Scanner scan=new Scanner();
+	//var name input
+	if(input.equals("yes")){
+	    social++;
+	}
+	else if(input.equals("no")){
+	    social--;
+	}
     }
-    
+	
 }
