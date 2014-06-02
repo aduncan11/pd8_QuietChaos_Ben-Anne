6bd953f706da6d8c6c555912ccef3b51d68ab479
Brothman <brothman7000@gmail.com>
2014-06-01 23:50:29 -0400

    Update SocialStory.java
    Added some scanners

--- SocialStory.java
+++ SocialStory.java
@@ -5,3 +5,30 @@ public class SocialStory extends StoryBit{
     }
     
 }
+
+//We will clean this up
+
+
+	Scanner scanner = (new Scanner(System.in)).useDelimiter(" ");
+	while (!(scanner.hasNext())) {} //waits for input	    
+	System.out.println("Press Ctrl + C to quit at any time.");
+	scanner = new Scanner(System.in); //intentional, to clear any input from the previous scanner instance
+	Character player = new Character (); //base player
+	Character opponent = new Character (); //base opponent
+	//Name
+	System.out.print("Enter name: ");
+	boolean input = false; //method to check input here is slightly different when actually looking for a specific input, rather than simply any input at all. Is there a more efficient method to do this?
+	String name = "";
+	while (!input) {
+	    name = (scanner.nextLine()).trim();
+	    if (!(name.equals(""))) {input = true;}
+	}
+	System.out.println(player.setBoldText + "Your name is " + name + "!" + "\n" + player.setPlainText); //Makes it bold
+	//Class
+	System.out.print("Select a class (0 - Random, 1 - Warrior, 2 - Wizard, 3 - Thief): ");
+	input = false;
+	String _class = "";
+	while (!input) {
+	    _class = (scanner.nextLine()).trim();
+	    if ((_class.equals("0")) || (_class.equals("1")) || (_class.equals("2")) || (_class.equals("3"))) {input = true;} //is there a more efficient method to do this line?
+	}
