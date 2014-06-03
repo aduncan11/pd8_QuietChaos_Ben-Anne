40d8727da892fb73a9cd35b0833bca79dead8792
aduncan11 <anneduncan11@gmail.com>
2014-06-03 14:00:11 -0400

    Update SocialStory.java
    finished, not debugged

--- SocialStory.java
+++ SocialStory.java
@@ -1,9 +1,15 @@
 public class SocialStory extends StoryBit{
 
-    public void run(Character c){ 
+    public void run(UserCharacter user){ 
 	int social=0; 
-	System.out.println("Your friend Veronica needs help with a problem...");
-	System.out.println("Hey, " +c.getName()+" Can you help me out with something?");
+	ComputerCharacter[] friends = new ComputerCharacter[5];
+	for(int x=0;x<friends.length;x++){
+	    friends[x]=user.getFriend();
+	}
+	int randfriend = Math.random()*5*10;
+	ComputerCharacter friend = friends[randfriend];
+	System.out.println("Your friend" + friend.getName() + "needs help with a problem...");
+	System.out.println("Hey, " +user.getName()+" Can you help me out with something?");
 	System.out.println("yes or no");
 	Scanner scan=new Scanner(System.in);
 	//var name input
@@ -13,9 +19,52 @@ public class SocialStory extends StoryBit{
 	}
 	if(input.equals("yes")){
 	    social++;
+	    String n1="";
+	    String n2="";
+	    if(friend.getGender().equals("male")){
+		n1="Julie";
+		n2="Cindy";
+	    }
+	    else{
+		n1="Kevin";
+		n2="Brandon";
+	    }
+	    System.out.println("Great! So, JProm is tomorrow and I don't have a date, but "+n1+" asked me if I wanted to go with zhim, but I told zhim I would have to think about it because I don't know if zhe likes me or if just wanted to go with me, and I don't know if "+n2+" is going to ask me tomorrow. What should I do?? ... \n
+1. Go with "+n1+" \n
+2. Go with "n2+" \n
+3. Go alone. \n
+4. Don't go. \n
+5. Come with me! I'm going with a group. \n
+(1,2,3,4, or 5)
+");
+	    input="";
+	    while(!(input.equals("1"))&&(!(input.equals("2")))&&(!(input.equals("3")))&&(!(input.equals("4")))&&(!(input.equals("5")))){
+		input+=scan.nextLine().trim();
+	    }
+	    if( input.equals("1") || input.equals("2")){
+		social--;
+	    }
+	    else if( input.equals("5")){
+		social++;
+	    }
 	}
 	else{
 	    social--;
+	    System.out.println("Wow. Way to be rude. ...");
+	    System.out.println("1. Sorry! I'm on my way to class right now! Call me later.");
+	    System.out.println("2. I really just don't care.");
+	    input="";
+	    while(!(input.equals("1") || input.equals("2"))){
+		input+=scan.nextLine().trim();
+	    }
+	    if( input.equals("1")){
+		social++;
+	    }
+	    else if( input.equals("5")){
+		social--;
+	    }
 	}
+	System.out.println("Social status change: "+social);
+	user.hangout(friend,social);
     }
 }
