d535a73335fa78e5a2687a12b0a4a11871f53ebc
Ben <brothman7000@gmail.com>
2014-06-09 11:47:29 -0400

    Created instance variables

--- SchoolStory.java
+++ SchoolStory.java
@@ -2,6 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class SchoolStory extends StoryBit {
+    
+
+
+    /*To make sure we don't repeat questions*/
+    protected EnglishStory _englishStory = new EnglishStory();
+    protected MathStory _mathStory = new MathStory();
+    protected HistoryStory _historyStory = new HistoryStory();
+    protected ScienceStory _scienceStory = new ScienceStory();
+
+    public SchoolStory () {
+	//Can't think of if we need a specific constructor but here just in case
+    }
+
+    Random rand = new Random();
+
+    
 
 
 
